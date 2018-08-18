585c67f4cda22ea426a709884fef454ea50b6792
aljarrett <41647796+aljarrett@users.noreply.github.com>
2018-08-17 20:19:28 -0700

    added files

---
+++ Chapter 2/Chapter 2 Lab Files/Chapter 2 Lab FIles/Circle.java
@@ -0,0 +1,15 @@
+
+public class Circle
+{
+  private double radius;
+
+  public Circle(double r)
+  {
+    radius = r;
+  }
+
+  public double getArea()
+  {
+    return Math.PI * radius * radius;
+  }
+}
